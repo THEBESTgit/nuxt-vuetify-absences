e8062524859ef125432f0e8a2c7ce07b0fc12624
Vera Zambrana Victor Mauricio <vmvzthebest@gmail.com>
2025-10-01 04:22:50 -0400

    feat: add main page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <absence-table />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: 'Roboto', sans-serif;
+}
+
+#__nuxt {
+  height: 100%;
+}
+</style>
